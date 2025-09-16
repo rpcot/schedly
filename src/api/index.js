cf64f9e7b57f3e222f68e0294d8c8ae11bc31053
rpcot <rpcotmail@mail.ru>
2025-09-17 00:56:00 +0300

    change api version

--- src/api/index.js
+++ src/api/index.js
@@ -159,7 +159,7 @@ app.use(cors({
 // }));
 
 app.get('/', (req, res) => {
-    res.status(200).json({ ok: true, status: 200, message: 'OK', version: '2.1.0' });
+    res.status(200).json({ ok: true, status: 200, message: 'OK', version: '2.1.1' });
 });
 
 app.get('/schedule/', upload.none(), async (req, res) => {
